470dc1e00a9941afdbb2fe70e1d2b1e28a03979c
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-02-25 15:50:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+#<sixie6e@paracosmoclast>
+#
+#pseudolayout
+#
+#
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
