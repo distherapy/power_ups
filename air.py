4b605d28d630dcfce2e44d8da14cfbf7905c38b8
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-03-02 10:18:19 -0500

    Update and rename wind.py to air.py

--- air.py
+++ air.py
@@ -7,10 +7,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import ezodf
 
-class wind():	
+class air():	
 	def __init__(self, name, types):
 		self.name = name
 		self.types = types
-
+init_data = pd.read_csv("/home/sixie6e/exploitation/basics/mapping/wdi_exp_growth_csv/air.csv")
+plt.show()
 #kwh0gain
 #kwh0cost
