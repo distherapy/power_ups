3ab545bd6d8eb409dedef168f15fc6088a081535
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-03-02 10:04:27 -0500

    Update water.py

--- water.py
+++ water.py
@@ -5,12 +5,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import ezodf
 
 class water():	
 	def __init__(self, name, types):
 		self.name = name
 		self.types = types
 
+init_data = pd.read_csv("/home/sixie6e/exploitation/basics/mapping/wdi_exp_growth_csv/water.csv/")
+plt.show()
 #kwh0gain
 #kwh0cost
