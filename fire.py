c404517f56b25ecdcd1dd86a215e8ef077a8ad56
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-03-02 10:19:57 -0500

    Update fire.py

--- fire.py
+++ fire.py
@@ -11,6 +11,9 @@ class fire():
 	def __init__(self, name, types):
 		self.name = name
 		self.types = types
+		
+init_data = pd.read_csv("/home/sixie6e/exploitation/basics/mapping/wdi_exp_growth_fire/fire.csv")
+plt.show()
 
 #kwh0gain
 #kwh0cost
