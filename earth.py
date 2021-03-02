30afc43569e6a51744f0a0588dd788ec5397fe39
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-03-02 10:18:51 -0500

    Update earth.py

--- earth.py
+++ earth.py
@@ -11,6 +11,7 @@ class earth():
 	def __init__(self, name, types):
 		self.name = name
 		self.types = types
-
+init_data = pd.read_csv("/home/sixie6e/exploitation/basics/mapping/wdi_exp_growth_earth/earth.csv")
+plt.show()
 #kwh0gain
 #kwh0cost
